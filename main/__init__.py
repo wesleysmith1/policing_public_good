b8f738da46a34802ad79333cfc2dbe226d168669
= <yelsew414@gmail.com>
2023-05-09 10:44:13 -0600

    added constants

--- main/__init__.py
+++ main/__init__.py
@@ -8,8 +8,59 @@ Your app description
 
 class C(BaseConstants):
     NAME_IN_URL = 'main'
-    PLAYERS_PER_GROUP = 5
-    NUM_ROUNDS = 1
+    PLAYERS_PER_GROUP = 6
+    NUM_ROUNDS = 10
+
+    CIVILIAN_START_BALANCE = 1000
+
+    """Number of defend tokens officer starts with"""
+    defend_token_total = 8
+
+    """Fine when convicted"""
+    civilian_fine_amount = 120
+    """number of grain earned per second of stealing"""
+    civilian_steal_rate = 13
+
+    """Probability that an intersection outcome will be reviewed"""
+    officer_review_probability = .1
+    """P punishment for officer if innocent civilian is punished"""
+    officer_reprimand_amount = 10
+
+    """Officer incomes. One for each group"""
+    officer_incomes = [180, 180, 180]
+
+    """ 
+    this is the size of the tokens and maps are defined. 
+    """
+    defend_token_size = 68 * 1.5
+    civilian_map_size = 200 * 1.5
+
+    """Probability innocent and guilty are calculated when the number of investigation tokens is >= this number"""
+    a_max = 6
+    """Guilty probability when """
+    beta = .9
+
+    """
+    Tutorial, game and results modal durations are defined here and passed to frontend
+    """
+    tutorial_duration_seconds = 1800
+    game_duration_seconds = 198
+    results_modal_seconds = 30
+    start_modal_seconds = 15
+
+    """
+    this defines how long a steal token remains on a map before resetting to the 'steal home'
+    """
+    steal_timeout_milli = 1000
+
+    """
+    Steal tokens positions defines the number of slots inside the 'steal home' rectangle. Steal tokens can be loaded or 
+    reset to any of the slots 
+    """
+    steal_token_slots = 20
+
+    officer_start_balance = 0
+    civilian_start_balance = 0
 
 
 class Subsession(BaseSubsession):
@@ -21,7 +72,49 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    x = models.FloatField(initial=0)
+    y = models.FloatField(initial=0)
+    map = models.IntegerField(initial=0)
+    last_updated = models.FloatField(blank=True)
+    roi = models.IntegerField(initial=0)
+    balance = models.FloatField(initial=C.CIVILIAN_START_BALANCE)
+    harvest_status = models.IntegerField(initial=0)
+    harvest_screen = models.BooleanField(initial=True)
+    income = models.IntegerField(initial=40)
+    steal_start = models.IntegerField(initial=0)
+    steal_count = models.IntegerField(initial=0)
+    victim_count = models.IntegerField(initial=0)
+    steal_total = models.FloatField(initial=0)
+    victim_total = models.FloatField(initial=0)
+    ready = models.BooleanField(initial=False)
+
+    def is_officer(self):
+        """officer always has id_in_group of 1"""
+        return self.id_in_group == 1
+    
+
+class DefendToken(ExtraModel):
+    group = models.Link(Group)
+    number = models.IntegerField()
+    map = models.IntegerField(initial=0)
+    x = models.FloatField(initial=0)
+    y = models.FloatField(initial=0)
+    x2 = models.FloatField(initial=0)
+    y2 = models.FloatField(initial=0)
+    last_updated = models.FloatField(blank=True)
+    slot = models.IntegerField(initial=-1)
+
+    def to_dict(self):
+        return {"number": self.number, "map": self.map, "x": self.x, "y": self.y}
+    
+
+class GameData(ExtraModel):
+    event_time = models.FloatField()
+    p = models.IntegerField(initial=0)
+    g = models.IntegerField(initial=0)
+    s = models.IntegerField(initial=0)
+    round_number = models.IntegerField(initial=0)
+    # jdata = models.
 
 
 # PAGES
